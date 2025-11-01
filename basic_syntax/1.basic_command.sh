8191e5e04e6a5ba5d78259ab7488ea21283d8baf
std-freejia <std.freejia@gmail.com>
2025-11-01 17:27:09 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -1,3 +1,9 @@
 
 # git add . 모든 변경사항을 staging area에 이동한다 
 git add . 
+
+# git commit -m "메시지" staging area에 있는 변경사항을 확정짓고, commit ID를 생성한다
+git commit -m "메시지"
+
+# 원격 repo에 main브랜치 변경사항 (commit ID) 를 업로드 
+git push origin main
